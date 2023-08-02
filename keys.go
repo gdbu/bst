46dcee277238237a0950b1f0210e6c1f8b0b8ab9
Josh Montoya <josh@montoya.io>
2023-08-02 14:04:56 -0700

    use Store as backend for Keys

--- keys.go
+++ keys.go
@@ -1,123 +1,34 @@
 package bst
 
-import "sort"
-
 // NewKeys a new Keys instance
 func NewKeys(keys ...string) *Keys {
-	s := makeKeys(keys)
-	return &s
-}
-
-func makeKeys(keys []string) (k Keys) {
-	sz := len(keys)
-	if sz == 0 {
-		sz = 8
+	var k Keys
+	k.Store = makeStore(nil)
+	for _, key := range keys {
+		k.Store.Set(key, struct{}{})
 	}
 
-	k.s = make([]string, len(keys), sz)
-	copy(k.s, keys)
-	sort.Strings(k.s)
-	return
+	return &k
 }
 
 // Keys is a Keys Stringset
 type Keys struct {
-	s []string
+	Store
 }
 
 // Set will place a key
 func (k *Keys) Set(key string) {
-	index, match := k.getIndex(key)
-	if match {
-		return
-	}
-
-	first := k.s[:index]
-	second := append([]string{key}, k.s[index:]...)
-	k.s = append(first, second...)
+	k.Store.Set(key, struct{}{})
 }
 
 // UsSet will remove a key
 func (k *Keys) Unset(key string) {
-	index, match := k.getIndex(key)
-	if !match {
-		return
-	}
-
-	first := k.s[:index]
-	second := k.s[index+1:]
-	k.s = append(first, second...)
-}
-
-// Has will return if a key exists
-func (k *Keys) Has(key string) (has bool) {
-	_, has = k.getIndex(key)
-	return
-}
-
-// Len will return the keys length
-func (k *Keys) Len() (n int) {
-	return len(k.s)
-}
-
-// Len will return the keys length
-func (k *Keys) Slice() (s []string) {
-	s = make([]string, len(k.s))
-	copy(s, k.s)
-	return
+	k.Store.Unset(key)
 }
 
 // Len will return the keys length
 func (k *Keys) ForEach(fn func(string) (end bool)) (ended bool) {
-	for _, k := range k.s {
-		if ended = fn(k); ended {
-			return
-		}
-	}
-
-	return
-}
-
-func (k *Keys) getIndex(key string) (index int, match bool) {
-	if len(k.s) == 0 {
-		return
-	}
-
-	start := 0
-	end := len(k.s) - 1
-	index = len(k.s) / 2
-
-	for {
-		ref := k.s[index]
-		switch {
-		case key == ref:
-			match = true
-			return
-		case key < ref:
-			end = index
-		case key > ref:
-			start = index
-		}
-
-		switch {
-		case start == end:
-			if key > ref {
-				index++
-			}
-
-			return
-		case end-start > 1:
-			index = (start + end) / 2
-		case start == index:
-			start++
-			index++
-		case end == index:
-			end--
-			index--
-		}
-	}
-}
-
-func (k *Keys) getKey(index int) string {
-	return k.s[index]
+	return k.Store.ForEach(func(key string, _ interface{}) (end bool) {
+		return fn(key)
+	})
 }
