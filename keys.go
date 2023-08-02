efaeacb6461ecd3fdec96734ec711535255563b5
Josh Montoya <josh@montoya.io>
2023-08-02 14:08:21 -0700

    remove non-needed keys

--- keys.go
+++ keys.go
@@ -21,11 +21,6 @@ func (k *Keys) Set(key string) {
 	k.Store.Set(key, struct{}{})
 }
 
-// UsSet will remove a key
-func (k *Keys) Unset(key string) {
-	k.Store.Unset(key)
-}
-
 // Len will return the keys length
 func (k *Keys) ForEach(fn func(string) (end bool)) (ended bool) {
 	return k.Store.ForEach(func(key string, _ interface{}) (end bool) {
