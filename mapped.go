2bf22b3babf638575b54c330abf190d29e77a394
Josh Montoya <josh@montoya.io>
2024-08-20 11:55:00 -0700

    upgrade mappedslice to v0.5.0

--- mapped.go
+++ mapped.go
@@ -4,9 +4,9 @@ import (
 	"github.com/itsmontoya/mappedslice"
 )
 
-func NewMapped[K any, V any](filepath string, compare func(K, K) int, kvs ...KV[K, V]) (out *Mapped[K, V], err error) {
+func NewMapped[K any, V any](filepath string, initialCapacity int64, compare func(K, K) int, kvs ...KV[K, V]) (out *Mapped[K, V], err error) {
 	var m Mapped[K, V]
-	if m.s, err = mappedslice.New[KV[K, V]](filepath); err != nil {
+	if m.s, err = mappedslice.New[KV[K, V]](filepath, initialCapacity); err != nil {
 		return
 	}
 
