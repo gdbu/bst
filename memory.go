ae965e5bbdade769cdd80afefe40c34fe13f7fa8
Josh Montoya <josh@montoya.io>
2024-08-24 11:20:50 -0700

    implement in memory type

---
+++ memory.go
@@ -0,0 +1,14 @@
+package bst
+
+func NewMemory[K any, V any](filepath string, initialCapacity int64, compare func(K, K) int, kvs ...KV[K, V]) (out *Memory[K, V], err error) {
+	var m Memory[K, V]
+	m.s = make(sliceBackend[K, V], 0, initialCapacity)
+	m.Raw = makeRaw(compare, &m.s, kvs)
+	out = &m
+	return
+}
+
+type Memory[K any, V any] struct {
+	s sliceBackend[K, V]
+	Raw[K, V]
+}
